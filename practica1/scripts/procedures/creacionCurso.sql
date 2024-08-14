197b6f6fb44ea979d510b92c7dbecf958d99f7a0
jorge <jorgeperez@inlandlogistics.co>
2024-08-13 18:01:26 -0600

    procedimiento creacion de curso

---
+++ practica1/scripts/procedures/creacionCurso.sql
@@ -0,0 +1,68 @@
+USE BD2;
+
+
+CREATE PROCEDURE PR5 
+	@CodCourse INT,
+	@Name NVARCHAR(max),
+	@CreditsRequired INT
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION;
+	
+		IF PATINDEX('%[^a-zA-Z ]%', @Name) > 0
+		BEGIN
+			-- Si el nombre del curso no lleva el formato correcto, devolver un mensaje de error
+	        RAISERROR('El nombre del curso es invalido, solo puede llevar letras y espacios.', 16, 1);
+	       	ROLLBACK TRANSACTION;
+	        RETURN; -- Terminar la ejecución del procedimiento
+		END
+		
+	
+		IF EXISTS (SELECT 1 FROM [practica1].[Course] WHERE CodCourse = @CodCourse)
+		BEGIN 
+			-- Si el curso ya existe, devolver un mensaje de error
+	        RAISERROR('Ya existe un curso con el codigo proporcionado.', 16, 1);
+	       	ROLLBACK TRANSACTION;
+	        RETURN; -- Terminar la ejecución del procedimiento
+		END
+		
+		INSERT INTO [practica1].[Course] (CodCourse, Name, CreditsRequired) VALUES (@CodCourse, @Name, @CreditsRequired);
+		
+		COMMIT TRANSACTION;
+		
+		PRINT('Curso creado exitosamente.');
+	
+	END TRY
+	BEGIN CATCH
+		IF @@TRANCOUNT > 0
+		BEGIN
+			ROLLBACK TRANSACTION;
+		END
+		
+		-- Devolver el error capturado
+        DECLARE @ErrorMessage NVARCHAR(4000);
+        DECLARE @ErrorSeverity INT;
+        DECLARE @ErrorState INT;
+
+        SELECT 
+            @ErrorMessage = ERROR_MESSAGE(),
+            @ErrorSeverity = ERROR_SEVERITY(),
+            @ErrorState = ERROR_STATE();
+
+        RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
+	END CATCH
+END
+
+EXEC PR5
+	@CodCourse = 45,
+	@Name = 'Teoria de Paradigmas',
+	@CreditsRequired = 130
+;
+
+
+IF OBJECT_ID('PR5', 'P') IS NOT NULL
+BEGIN
+    DROP PROCEDURE PR5;
+END;
+	
