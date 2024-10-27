e12d7b6ad34a814044ccb361c8bbe3a613d7eb55
Sergio Ralda <sergioralda440@gmail.com>
2024-10-27 01:16:43 -0600

    view_info

---
+++ proyecto2/frontend/src/pages/view_data.jsx
@@ -0,0 +1,9 @@
+import Dash from "../layout/dashboardHome";
+
+export default function ViewData() {
+  return (
+    <Dash>
+        <h1>View Data</h1>
+    </Dash>
+  );
+}
--- proyecto2/frontend/src/router.jsx
+++ proyecto2/frontend/src/router.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Dash from "./layout/dashboardHome";
+import ViewData from "./pages/view_data";
 
 function Aplicacion() {
   return (
     <Routes>
-      <Route path="/" element={<Dash />} />
+      <Route path="/" element={<ViewData></ViewData>} />
     </Routes>
   );
 }
